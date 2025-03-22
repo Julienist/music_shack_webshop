db6f7c4a09cd34e3fb83c23918c71b461e10ad50
Julien Mijnders <s1142531@student.hsleiden.nl>
2025-03-22 13:42:22 +0100

    import fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatIconModule
+    MatIconModule,
+    RouterModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
