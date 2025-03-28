24498a484a49be936e21de547267db5661f6b1b3
Julien Mijnders <s1142531@student.hsleiden.nl>
2025-03-28 22:01:35 +0100

    code geoptimaliseerd met lombok lib.
    - Getters
    - Setters
    - NoArgsConstruct
    
    en Table name = Order

--- src/main/java/com/duckstudios/webshopapi/models/OrderEntity.java
+++ src/main/java/com/duckstudios/webshopapi/models/OrderEntity.java
@@ -2,11 +2,16 @@ package com.duckstudios.webshopapi.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "\"Order\"")
 public class OrderEntity {
     @Id
     @GeneratedValue
@@ -22,53 +27,51 @@ public class OrderEntity {
 
     private BigDecimal totalPrice;
 
-
-    public OrderEntity(){}
-
     public OrderEntity(CustomUser customUser, LocalDateTime orderDate, Enum orderStatus, BigDecimal totalPrice) {
         this.customUser = customUser;
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.totalPrice = totalPrice;
     }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public CustomUser getCustomUser() {
-        return customUser;
-    }
-
-    public void setCustomUser(CustomUser customUser) {
-        this.customUser = customUser;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public Enum getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(Enum orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(BigDecimal totalPrice) {
-        this.totalPrice = totalPrice;
-    }
 }
+
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public CustomUser getCustomUser() {
+//        return customUser;
+//    }
+//
+//    public void setCustomUser(CustomUser customUser) {
+//        this.customUser = customUser;
+//    }
+//
+//    public LocalDateTime getOrderDate() {
+//        return orderDate;
+//    }
+//
+//    public void setOrderDate(LocalDateTime orderDate) {
+//        this.orderDate = orderDate;
+//    }
+//
+//    public Enum getOrderStatus() {
+//        return orderStatus;
+//    }
+//
+//    public void setOrderStatus(Enum orderStatus) {
+//        this.orderStatus = orderStatus;
+//    }
+//
+//    public BigDecimal getTotalPrice() {
+//        return totalPrice;
+//    }
+//
+//    public void setTotalPrice(BigDecimal totalPrice) {
+//        this.totalPrice = totalPrice;
+//    }
+//}
