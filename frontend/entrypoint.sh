44908ecfe23abacba75fa85a525d59029bb3cd34
julien <s1142531@student.hsleiden.nl>
2026-07-07 22:47:03 +0200

    - env.js added - added writing env.js into workflow - entrypoint.sh syntax fix - added env.js into index.html as script.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,13 +3,13 @@
 cat > /usr/share/nginx/html/assets/env.js << EOF
 (function(window) {
     window.__env = {
-        baseApiUrl: '${API_URL}'
+        apiUrl: '${API_URL}'
     };
 
 }(this));
 EOF
 
 # nginx.conf genereren vanuit template
-envsubst '${API_URL} ${ALLOWED_ORIGIN} ${BACKEND_URL}' < etc/ngin/templates/nginx.conf.template > etc/nginx/conf.d/default.conf
+envsubst '${API_URL} ${ALLOWED_ORIGIN} ${BACKEND_URL}' < etc/nginx/templates/nginx.conf.template > etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
--- frontend/src/assets/env.js
+++ frontend/src/assets/env.js
@@ -1,5 +1,5 @@
-(function(window) {
-    window.__env = {
-      baseApiUrl: 'http://localhost:8080/api'
-    };
-}(this));
+(function (window) {
+  window.__env = {
+    apiUrl: "http://localhost:8080/api",
+  };
+})(this);
